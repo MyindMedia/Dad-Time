d6d9bb2855b550dab20b981a7db96f24c2f2f401
MyindMedia <info@myindmedia.org>
2025-11-26 13:14:56 -0800

    Favicon: switch to dad_time_text_logo; Maps loader and GPS route drawing; privacy/terms routes; manual expense form

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import faviconUrl from '../assets/dad_time_text_logo.png'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Dynamically set favicon and touch icon to bundled asset
+try {
+  const linkIcon = document.querySelector<HTMLLinkElement>('link[rel="icon"]')
+  if (linkIcon) linkIcon.href = faviconUrl
+  const appleIcon = document.querySelector<HTMLLinkElement>('link[rel="apple-touch-icon"]')
+  if (appleIcon) appleIcon.href = faviconUrl
+} catch {}
